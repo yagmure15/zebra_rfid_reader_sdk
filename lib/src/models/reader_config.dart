39668c1b4a4c61438108afc0070277bba9a2c9bb
Bordatech VSTS <vsts@bordech.onmicrosoft.com>
2024-04-01 02:54:35 +0300

    reader config modal refactor
    copyWith method added

--- lib/src/models/reader_config.dart
+++ lib/src/models/reader_config.dart
@@ -34,4 +34,31 @@ class ReaderConfig {
       'isDynamicPowerEnable': isDynamicPowerEnable,
     };
   }
+
+  ReaderConfig copyWith({
+    int? antennaPower,
+    BeeperVolume? beeperVolume,
+    bool? isDynamicPowerEnable,
+  }) {
+    return ReaderConfig(
+      antennaPower: antennaPower ?? this.antennaPower,
+      beeperVolume: beeperVolume ?? this.beeperVolume,
+      isDynamicPowerEnable: isDynamicPowerEnable ?? this.isDynamicPowerEnable,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is ReaderConfig &&
+        antennaPower == other.antennaPower &&
+        beeperVolume == other.beeperVolume &&
+        isDynamicPowerEnable == other.isDynamicPowerEnable;
+  }
+
+  @override
+  int get hashCode {
+    return antennaPower.hashCode ^ beeperVolume.hashCode ^ isDynamicPowerEnable.hashCode;
+  }
 }
